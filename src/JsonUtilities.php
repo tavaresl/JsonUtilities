bdb120e9de57055b97f28df475da7cd15e1a678a
Lucas Tavares <lucastavaresdarocha@gmail.com>
2017-01-07 16:36:37 -0200

    tests

--- src/JsonUtilities.php
+++ src/JsonUtilities.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Class JsonUtilities, which provides an interface between your code and json specs.<br>
- * Basically, what JsonUtilities does is parse the given parameter, which can be of any type, to a json format, <br>
- * encoded in UTF-8. Numeric strings will be parsed to their int or float equivalent values, <br>
- * and boolean will be parsed to integer.
- */
+
 abstract class JsonUtilities
 {
     public static function parse($source)
@@ -19,19 +14,22 @@ abstract class JsonUtilities
     private static function fromArray($array)
     {
         $string = '[';
+
         for ($i = 0, $separator = ''; $i < count($array); $i++, $separator = ',') {
             $string .= $separator . self::fromObject($array[$i]);
         }
+
         $string .= ']';
+
         return $string;
     }
     private static function fromObject($object)
     {
         $reflection = new ReflectionClass($object);
         $properties = $reflection->getProperties(ReflectionProperty::IS_PRIVATE);
-        $vector = (array)$object;
-        $values = array_values($vector);
-        $string = '{';
+        $vector     = (array)$object;
+        $values     = array_values($vector);
+        $string     = '{';
 
         for ($i = 0, $separator = ''; $i < count($properties); $i++, $separator = ',') {
             $key = (string)$properties[$i]->name;
@@ -50,6 +48,7 @@ abstract class JsonUtilities
                 $value .= self::fromArray($values[$i]);
             else
                 $value .= "\"{$values[$i]}\"";
+
             $string .= $separator . "\"{$key}\":{$value}";
         }
 
